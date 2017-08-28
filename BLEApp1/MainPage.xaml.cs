18ff216f5d6f940999622abca35eb0328528b464
Dave Harmon Jr <Dave.HarmonJr@KFCI.local>
2017-08-28 08:44:17 -0400

    Add project files.

---
+++ BLEApp1/MainPage.xaml.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Popups;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using Windows.Devices.Enumeration;
+
+
+namespace BLEApp1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        String deviceID;
+        bool bleConnected = false;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+
+        private void maintextoutput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var grid = (Grid)VisualTreeHelper.GetChild(maintextoutput, 0);
+            for (var i = 0; i <= VisualTreeHelper.GetChildrenCount(grid) - 1; i++)
+            {
+                object obj = VisualTreeHelper.GetChild(grid, i);
+                if (!(obj is ScrollViewer)) continue;
+                ((ScrollViewer)obj).ChangeView(0.0f, ((ScrollViewer)obj).ExtentHeight, 1.0f,true);
+                break;
+            }
+        
+        }
+
+        private async void FindAdaFruitBLE()
+        {
+            TextBox outputTextbox = (TextBox)this.FindName("maintextoutput");
+
+            String findStuff = "System.DeviceInterface.Bluetooth.ServiceGuid:= \"{6e400001-b5a3-f393-e0a9-e50e24dcca9e}\" AND System.Devices.InterfaceEnabled:= System.StructuredQueryType.Boolean#True";
+            var devices = await DeviceInformation.FindAllAsync(findStuff);
+
+            foreach(DeviceInformation device in devices)
+            {
+                
+                deviceID = device.Id;
+
+                if(device.IsEnabled)
+                {
+                    outputTextbox.Text += "Found Paired Device. Attempting Connection.\n";
+                }
+                else
+                {
+                    outputTextbox.Text += "Can Not Find Paired Device. Please Pair in Windows Settings, Under Bluetooth.\n";
+                }
+            }
+
+            UARTEngine.Instance.DeviceConnectionUpdated += Instance_DeviceConnectionUpdated;
+            UARTEngine.Instance.ValueChangeCompleted += Instance_ValueChangeCompleted;
+            UARTEngine.Instance.InitializeServiceAsync(deviceID);
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            FindAdaFruitBLE();
+        }
+
+        private async void Instance_DeviceConnectionUpdated(bool isConnected, string error)
+        {
+            // Serialize UI update to the the main UI thread.
+            await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                if (error != null)
+                {
+                    ShowErrorDialog(error, "Connect error.");
+                   
+                }
+
+                if (isConnected)
+                {
+                    bleConnected = true;
+                    maintextoutput.Text += "Connected To KCi STAR.\nPress Enter in the textfield below to start. (If a callbox you must press the program button on the board first.)\n";
+                    
+                }
+                else
+                {
+                    bleConnected = false;
+                    maintextoutput.Text += "Disconnected From Bluetooth Module. Check Bluetooth Nodule LED's.\nPress Connect to resume communications after resolving connection issues.\n";
+                }
+            });
+        }
+
+        private async void Instance_ValueChangeCompleted(String data)
+        {
+            // Serialize UI update to the the main UI thread.
+            await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+
+               
+
+                maintextoutput.Text += data;
+                //maintextoutput.SelectionStart = maintextoutput.Text.Length;
+                maintextoutput.Select(maintextoutput.Text.Length-1, 1);
+
+            });
+        }
+
+        private async void ShowErrorDialog(string message, string title)
+        {
+            await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, async () =>
+            {
+                var dialog = new MessageDialog(message, title);
+                await dialog.ShowAsync();
+            });
+        }
+
+        private void inputText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+            
+        }
+
+        private void inputText_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if(e.Key == Windows.System.VirtualKey.Enter && bleConnected)
+            {
+                UARTEngine.Instance.SendTXValue((inputText.Text) + '\r');
+                inputText.Text = "";
+                e.Handled = true;
+            }
+            else if(e.Key == Windows.System.VirtualKey.Enter && !bleConnected)
+            {
+                maintextoutput.Text += "\nError: Not connected to Bluetooth Module. Please reconnect.\n";
+                e.Handled = true;
+            }
+        }
+    }
+}
---
+++ BLEApp1/UARTEngine.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Enumeration;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using Windows.Storage.Streams;
+
+//UUIDS
+//RX ID =           6e400003-b5a3-f393-e0a9-e50e24dcca9e
+//TX ID =           6e400002-b5a3-f393-e0a9-e50e24dcca9e
+//UART Service ID = 6e400001-b5a3-f393-e0a9-e50e24dcca9e
+
+
+
+namespace BLEApp1
+{
+
+    
+
+    //Declare Event Handlers for receiving data from the BLE UART Device & Connection Status
+    public delegate void ValueChangeCompletedHandler(String charIn);
+    public delegate void DeviceConnectionUpdatedHandler(bool isConnected, string error);
+
+
+    public class UARTEngine
+    {
+        //GUIDS
+        Guid rxGUID = new Guid("6e400003-b5a3-f393-e0a9-e50e24dcca9e");
+        Guid txGUID = new Guid("6e400002-b5a3-f393-e0a9-e50e24dcca9e");
+        Guid uartGUID = new Guid("6e400001-b5a3-f393-e0a9-e50e24dcca9e");
+
+        //BLE GATT Objects for Receiving/Sending Data
+        private GattDeviceService _service = null;
+        private GattCharacteristic _characteristicRX = null;
+        private GattCharacteristic _characteristicTX = null;
+
+        private static UARTEngine _instance = new UARTEngine();
+
+        //Allows other parts of the program to access the instance of this class
+        public static UARTEngine Instance
+        {
+            get { return _instance; }
+        }
+
+        public event ValueChangeCompletedHandler ValueChangeCompleted;
+        public event DeviceConnectionUpdatedHandler DeviceConnectionUpdated;
+
+        public void Deinitialize()
+        {
+
+            //Remove event handler and set characteristic to null
+            if(_characteristicRX != null)
+            {
+                _characteristicRX.ValueChanged -= Oncharacteristic_ValueChanged;
+                _characteristicRX = null;
+            }
+
+            //Set TX to null
+            if(_characteristicTX != null)
+            {
+                _characteristicTX = null;
+            }
+
+            if(_service != null)
+            {
+                _service.Device.ConnectionStatusChanged -= OnConnectionStatusChanged;
+            }
+
+        }
+
+        public async void InitializeServiceAsync(string deviceID)
+        {
+            try
+            {
+                Deinitialize();
+                _service = await GattDeviceService.FromIdAsync(deviceID);
+
+                if(_service != null)
+                {
+                    //Check to make sure we arent already connected
+                    if(DeviceConnectionUpdated != null && (_service.Device.ConnectionStatus == BluetoothConnectionStatus.Connected))
+                    {
+                        DeviceConnectionUpdated(true, null);
+                    }
+
+                    _service.Device.ConnectionStatusChanged += OnConnectionStatusChanged;
+
+                    //Setup RX Characteristic and register event handler
+                    _characteristicRX = _service.GetCharacteristics(rxGUID)[0];
+                    _characteristicRX.ValueChanged += Oncharacteristic_ValueChanged;
+
+                    var currentDescriptorValue = await _characteristicRX.ReadClientCharacteristicConfigurationDescriptorAsync();
+                    if ((currentDescriptorValue.Status != GattCommunicationStatus.Success) ||
+                    (currentDescriptorValue.ClientCharacteristicConfigurationDescriptor != GattClientCharacteristicConfigurationDescriptorValue.Notify))
+                    {
+                        // most likely we never get here, though if for any reason this value is not Notify, then we should really set it to be
+                        await _characteristicRX.WriteClientCharacteristicConfigurationDescriptorAsync(GattClientCharacteristicConfigurationDescriptorValue.Notify);
+                    }
+
+                    //Setup TX Characteristic
+                    _characteristicTX = _service.GetCharacteristics(txGUID)[0];
+
+                }
+
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine("ERROR: Accessing your device failed." + Environment.NewLine + e.Message);
+
+                if (DeviceConnectionUpdated != null)
+                {
+                    DeviceConnectionUpdated(false, "Accessing device failed: " + e.Message);
+                }
+            }
+
+
+        }
+
+        //Send TX Value
+        public async void SendTXValue(String data)
+        {
+
+            IBuffer sendBuf = Encoding.ASCII.GetBytes(data).AsBuffer();
+
+            
+           await _characteristicTX.WriteValueAsync(sendBuf,GattWriteOption.WriteWithoutResponse);
+        }
+            
+
+
+
+        //Event Handlers for RX Value Changed
+        private void Oncharacteristic_ValueChanged(GattCharacteristic sender, GattValueChangedEventArgs args)
+        {
+
+            //Allocate byte buffer for new data received
+            var data = new byte[args.CharacteristicValue.Length];
+
+            //Put new data into the buffer
+            DataReader.FromBuffer(args.CharacteristicValue).ReadBytes(data);
+
+            //Convert received data into a string
+            String rxString = System.Text.Encoding.ASCII.GetString(data);
+
+            //Send string to the event
+            ValueChangeCompleted(rxString);
+
+        }
+
+        //Event Handler for Connection Value Changed
+        private void OnConnectionStatusChanged(BluetoothLEDevice sender, object args)
+        {
+            if (sender.ConnectionStatus == BluetoothConnectionStatus.Connected)
+            {
+                System.Diagnostics.Debug.WriteLine("Connected");
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("Disconnected");
+            }
+
+            if (DeviceConnectionUpdated != null)
+            {
+                DeviceConnectionUpdated(sender.ConnectionStatus == BluetoothConnectionStatus.Connected, null);
+            }
+        }
+    }
+}
